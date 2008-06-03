464d441d2a8a4fc3d72cede27ddf4fcc4ed2595d
Thomas M. Andersson <ltmandersson@gmail.com>
2008-06-03 19:37:49 +0000

    * Added first failing test to demonstrate that nunit is working. Now all is set to start programming on our first user story for getting the very basic "Inbox" functionality working.
    git-svn-id: svn://localhost/trunk@4 92cadb52-44c2-2b4a-bfa8-babc7142421a

---
+++ FastGTD/FirstTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace FastGTD
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void FirstTest_xxx()
+        {
+            Assert.Fail();
+        }
+    }
+}

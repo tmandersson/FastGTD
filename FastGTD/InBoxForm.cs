e5eff0cacba8549bc033dbb26b20a293613ba089
Thomas M. Andersson <ltmandersson@gmail.com>
2008-06-15 11:46:27 +0000

    * Inbox items can now be added by pressing Enter key.
    git-svn-id: svn://localhost/trunk@9 92cadb52-44c2-2b4a-bfa8-babc7142421a

--- FastGTD/InBoxForm.cs
+++ FastGTD/InBoxForm.cs
@@ -7,9 +7,25 @@ namespace FastGTD
         public InBoxForm()
         {
             InitializeComponent();
+
+            textBoxNewItem.KeyDown += KeyDownHandler;
+        }
+
+        public void KeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                AddInboxItem();
+                e.SuppressKeyPress = true;
+            }
         }
 
         private void buttonAdd_Click(object sender, System.EventArgs e)
+        {
+            AddInboxItem();
+        }
+
+        private void AddInboxItem()
         {
             string new_item = textBoxNewItem.Text;
             listViewInBoxItems.Items.Add(new_item);
--- FastGTD/InBoxFormTests.cs
+++ FastGTD/InBoxFormTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Windows.Forms;
+using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 
 namespace FastGTD
@@ -31,6 +32,25 @@ namespace FastGTD
             Assert.That(form.listViewInBoxItems.Items.Count, Is.EqualTo(2));
             Assert.That(form.listViewInBoxItems.Items[0].Text, Is.EqualTo(new_inbox_item));
             Assert.That(form.listViewInBoxItems.Items[1].Text, Is.EqualTo(new_inbox_item2));
+        }
+
+        [Test]
+        public void AddingInBoxItemWithEnterKey()
+        {
+            InBoxForm form = new InBoxForm();
+            form.Show(); // TODO: Eliminate need to show dialog when testing.
+
+            Assert.That(form.listViewInBoxItems.Items.Count, Is.EqualTo(0));
+
+            string new_inbox_item = "foo";
+            form.textBoxNewItem.Text = new_inbox_item;
+
+            form.KeyDownHandler((object)this, new KeyEventArgs(Keys.Space));
+            Assert.That(form.listViewInBoxItems.Items.Count, Is.EqualTo(0));
+
+            form.KeyDownHandler((object) this, new KeyEventArgs(Keys.Enter));
+            Assert.That(form.listViewInBoxItems.Items.Count, Is.EqualTo(1));
+            Assert.That(form.listViewInBoxItems.Items[0].Text, Is.EqualTo(new_inbox_item));
         }
     }
 }

d40144f35ec7c00bdc5839f8548c12cb02a89d27
Thomas M. Andersson <ltmandersson@gmail.com>
2008-08-10 16:10:36 +0000

    Try to refactor to MVP: * Trying to implement AddItemAction by small TDD steps.
    git-svn-id: svn://localhost/trunk@32 92cadb52-44c2-2b4a-bfa8-babc7142421a

---
+++ FastGTD.Tests/InBoxModelTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace FastGTD.Tests
+{
+    [TestFixture]
+    public class InBoxModelTests
+    {
+        [Test]
+        public void EmptyModel()
+        {
+            IInBoxModel model = new InBoxModel();
+            Assert.That(model.InboxItems.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AddingInBoxItem()
+        {
+            IInBoxModel model = new InBoxModel();
+
+            model.AddInboxItem("foo");
+            Assert.That(model.InboxItems.Count, Is.EqualTo(1));
+            Assert.That(model.InboxItems[0], Is.EqualTo("foo"));
+
+            model.AddInboxItem("bar");
+            Assert.That(model.InboxItems.Count, Is.EqualTo(2));
+            Assert.That(model.InboxItems[1], Is.EqualTo("bar"));
+        }
+    }
+}

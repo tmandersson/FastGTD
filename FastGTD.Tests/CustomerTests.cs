ee1bc62f722ad96707ffa41e1863367aeac07d79
Thomas M. Andersson <ltmandersson@gmail.com>
2009-04-16 17:43:56 +0000

    * Added personal TODO file. (In swedish since it's only my notes.) * Removed some TODOs in the code. From now on these should not be put in the code. The problem should be fixed right away or put on the TODO file which I work on, meaning it will soon be fixed. * Removed unused test which was for a defect which doesn't matter much. (That items with the same name was deleted when one was deleted.)
    git-svn-id: svn://localhost/trunk@75 92cadb52-44c2-2b4a-bfa8-babc7142421a

--- FastGTD.Tests/CustomerTests.cs
+++ FastGTD.Tests/CustomerTests.cs
@@ -10,9 +10,6 @@ namespace FastGTD.Tests
         [Test]
         public void NewInBoxItemIsSaved()
         {
-            // TODO: Move tests into main assembly again? No benefit in having it separate?
-            // TODO: Model and Form should not be responsible for saving etc. Refactor test and code.
-
             string ITEM = Guid.NewGuid().ToString();
 
             FastGTDApp app = FastGTDApp.StartNewTestApplication();
@@ -45,29 +42,6 @@ namespace FastGTD.Tests
             Assert.That(app2.InboxModel.Items, Has.Count(1));
             Assert.That(app2.InboxModel.Items, Has.Member(item2));
             Assert.That(app2.InboxModel.Items, Has.No.Member(item));
-            app2.Close();
-        }
-
-        [Test, Ignore("Not done")]
-        public void RemovingItemShouldNotDeleteOtherItemsWithSameName()
-        {
-            string ITEM = Guid.NewGuid().ToString();
-            string ITEM2 = Guid.NewGuid().ToString();
-
-            FastGTDApp app = FastGTDApp.StartNewTestApplication();
-            app.InboxModel.ClearItems();
-            Assert.That(app.InboxForm.ListViewItems.Count, Is.EqualTo(0));
-            var item = app.InboxModel.CreateItem(ITEM);
-            var existing_item = app.InboxModel.CreateItem(ITEM);
-            app.InboxModel.CreateItem(ITEM);
-            var item2 = app.InboxModel.CreateItem(ITEM2);
-            app.InboxModel.RemoveItem(item);
-            app.Close();
-
-            FastGTDApp app2 = FastGTDApp.StartNewTestApplication();
-            Assert.That(app2.InboxModel.Items, Has.Count(3));
-            Assert.That(app2.InboxModel.Items, Has.Member(item2));
-            Assert.That(app2.InboxModel.Items, Has.Member(existing_item));
             app2.Close();
         }
     }

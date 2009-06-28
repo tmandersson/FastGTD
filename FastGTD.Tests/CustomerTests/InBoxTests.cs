bc71349ef669c5773953cbcbc60adc80b4fceeb5
Thomas M. Andersson <ltmandersson@gmail.com>
2009-06-28 18:03:36 +0000

    * Placed customer tests in dedicated folder.
    git-svn-id: svn://localhost/trunk@113 92cadb52-44c2-2b4a-bfa8-babc7142421a

--- FastGTD.Tests/CustomerTests/InBoxTests.cs
+++ FastGTD.Tests/CustomerTests/InBoxTests.cs
@@ -2,7 +2,7 @@ using System;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 
-namespace FastGTD.Tests
+namespace FastGTD.Tests.CustomerTests
 {
     [TestFixture, Explicit, Category("slow")]
     public class InBoxTests

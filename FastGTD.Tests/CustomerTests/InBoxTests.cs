e30c8fcef94a6acdb68e997c8e99dfa1ca257478
Thomas M. Andersson <ltmandersson@gmail.com>
2009-06-29 17:28:57 +0000

    * Half-done changes.
    git-svn-id: svn://localhost/trunk@114 92cadb52-44c2-2b4a-bfa8-babc7142421a

--- FastGTD.Tests/CustomerTests/InBoxTests.cs
+++ FastGTD.Tests/CustomerTests/InBoxTests.cs
@@ -1,4 +1,5 @@
 using System;
+using FastGTD.DataTransfer;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 
@@ -41,6 +42,53 @@ namespace FastGTD.Tests.CustomerTests
             Assert.That(app2.InboxModel.Items, Has.Member(item2));
             Assert.That(app2.InboxModel.Items, Has.No.Member(item));
             app2.Close();
+        }
+
+        private static FastGTDApp CreateAndStartTestApp()
+        {
+            var app = FastGTDApp.Create();
+            app.ShowStartForm();
+            return app;
+        }
+    }
+
+    [TestFixture, Explicit, Category("slow")]
+    public class XxxTests
+    {
+        private FastGTDApp _app;
+        private readonly string _item_name1 = Guid.NewGuid().ToString();
+        private readonly string _item_name2 = Guid.NewGuid().ToString();
+
+        [Test]
+        public void AddingInBoxItems()
+        {
+            GetAppWithEmptyInBox();
+            InBoxItem item1 = AddInBoxItem(_item_name1);
+            InBoxItem item2 = AddInBoxItem(_item_name2);
+            Assert.That(InBoxCount(), Is.EqualTo(2));
+            Assert.That(InBoxContains(item1), Is.True);
+            Assert.That(InBoxContains(item2), Is.True);
+        }
+
+        private InBoxItem AddInBoxItem(string item_name)
+        {
+            return _app.InboxModel.Add(item_name);
+        }
+
+        private int InBoxCount()
+        {
+            return _app.InboxModel.Items.Count;
+        }
+
+        private bool InBoxContains(InBoxItem item_name)
+        {
+            return _app.InboxModel.Items.Contains(item_name);
+        }
+
+        private void GetAppWithEmptyInBox()
+        {
+            _app = CreateAndStartTestApp();
+            _app.InboxModel.ClearItems();
         }
 
         private static FastGTDApp CreateAndStartTestApp()

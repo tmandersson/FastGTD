ae7241e205226f6e002ac75755c93c2142c8bf12
Thomas M. Andersson <ltmandersson@gmail.com>
2009-09-26 17:54:12 +0000

    * Updated batch-files with correct path to nunit after update of nunit.
    git-svn-id: svn://localhost/trunk@173 92cadb52-44c2-2b4a-bfa8-babc7142421a

--- check_coverage_all_tests.bat
+++ check_coverage_all_tests.bat
@@ -1,5 +1,5 @@
 @echo off
 devenv.exe FastGTD.sln /Rebuild
 del coverage.xml
-"Tools\PartCover .NET 2\PartCover.exe" --target "Tools\NUnit\bin\nunit-console.exe" --target-args "FastGTD.UnitTests\bin\Debug\FastGTD.UnitTests.dll FastGTD.CustomerTests\bin\Debug\FastGTD.CustomerTests.dll FastGTD.DataAccessTests\bin\Debug\FastGTD.DataAccessTests.dll" --include [FastGTD*]* --output coverage.xml
+"Tools\PartCover .NET 2\PartCover.exe" --target "Tools\NUnit\bin\net-2.0\nunit-console.exe" --target-args "FastGTD.UnitTests\bin\Debug\FastGTD.UnitTests.dll FastGTD.CustomerTests\bin\Debug\FastGTD.CustomerTests.dll FastGTD.DataAccessTests\bin\Debug\FastGTD.DataAccessTests.dll" --include [FastGTD*]* --output coverage.xml
 "Tools\PartCover .NET 2\PartCover.Browser.exe"
--- check_coverage_unit_tests.bat
+++ check_coverage_unit_tests.bat
@@ -1,5 +1,5 @@
 @echo off
 devenv.exe FastGTD.sln /Rebuild
 del coverage.xml
-"Tools\PartCover .NET 2\PartCover.exe" --target "Tools\NUnit\bin\nunit-console.exe" --target-args "FastGTD.UnitTests\bin\Debug\FastGTD.UnitTests.dll" --include [FastGTD*]* --output coverage.xml
+"Tools\PartCover .NET 2\PartCover.exe" --target "Tools\NUnit\bin\net-2.0\nunit-console.exe" --target-args "FastGTD.UnitTests\bin\Debug\FastGTD.UnitTests.dll" --include [FastGTD*]* --output coverage.xml
 "Tools\PartCover .NET 2\PartCover.Browser.exe"

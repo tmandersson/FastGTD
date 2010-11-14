3b06913e2fc6185f80f5dca5b689a423a1982c8d
Thomas M. Andersson <ltmandersson@gmail.com>
2010-11-14 11:43:28 +0100

    * Going to try to white end to end customer tests through the UI.

---
+++ FastGTD.CustomerTests/EndToEndUITests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using NUnit.Framework;
+using White.Core;
+
+namespace FastGTD.CustomerTests
+{
+    [TestFixture]
+    public class EndToEndUITests
+    {
+        [Test]
+        public void TryWhite()
+        {
+            var app = Application.Launch("FastGTD.exe");
+            //var inbox_window = app.GetWindow("");
+            
+            app.Kill();
+        }
+    }
+}

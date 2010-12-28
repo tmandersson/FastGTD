126bbf8976a3e294419b69f6046e01d078f7e3f3
Thomas M. Andersson <ltmandersson@gmail.com>
2010-12-28 08:47:01 +0100

    * First proper test with white. Adding item to inbox.

--- FastGTD.CustomerTests/EndToEndUITests.cs
+++ FastGTD.CustomerTests/EndToEndUITests.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using NUnit.Framework;
-using White.Core;
+using White.Core.UIItems;
+using White.Core.UIItems.Finders;
 using White.Core.UIItems.WindowItems;
+using White.Core.WindowsAPI;
+using Application = White.Core.Application;
 using Debug = System.Diagnostics.Debug;
 
 namespace FastGTD.CustomerTests
@@ -10,20 +14,31 @@ namespace FastGTD.CustomerTests
     public class EndToEndUITests
     {
         [Test]
-        public void TryWhite()
+        public void AddingItemToInbox()
         {
+            string new_item = Guid.NewGuid().ToString();
+            
             var app = Application.Launch("FastGTD.exe");
-            Thread.Sleep(5000);
-            //app.WaitWhileBusy();
-            //var windows = app.GetWindows();
-            //foreach (var w in windows)
-            //{
-            //    Debug.WriteLine(w.Title);
-            //}
-            //app.WaitWhileBusy();
-            //var inbox_window = app.GetWindow("InBox");
-            //inbox_window.Focus(DisplayState.Maximized);
-           
+            var inbox_window = app.GetWindow("InBox");
+            AddItem(inbox_window, new_item);
+            app.WaitWhileBusy();
+
+            AssertListHasItem(inbox_window, new_item);
+
+            app.Kill();
+        }
+
+        private static void AssertListHasItem(Window inbox_window, string new_item)
+        {
+            var list_view = (ListView) inbox_window.GetMultiple(SearchCriteria.ByControlType(typeof(ListView)))[0];
+            Assert.That(list_view.Rows, Has.Some.Matches<ListViewRow>(i => i.Cells[0].Text == new_item));
+        }
+
+        private static void AddItem(Window inbox_window, string new_item)
+        {
+            inbox_window.Focus();
+            inbox_window.Enter(new_item);
+            inbox_window.KeyIn(KeyboardInput.SpecialKeys.RETURN);
         }
     }
 }

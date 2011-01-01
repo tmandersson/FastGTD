482281d6bfc3ef2b9e66b38f3cc4c6284bcd3ed0
Thomas M. Andersson <ltmandersson@gmail.com>
2011-01-01 20:54:30 +0100

    * Added next end to end ui test.

--- FastGTD.CustomerTests/EndToEndUITests.cs
+++ FastGTD.CustomerTests/EndToEndUITests.cs
@@ -1,44 +1,72 @@
 ﻿using System;
-using System.Threading;
 using NUnit.Framework;
 using White.Core.UIItems;
 using White.Core.UIItems.Finders;
 using White.Core.UIItems.WindowItems;
 using White.Core.WindowsAPI;
 using Application = White.Core.Application;
-using Debug = System.Diagnostics.Debug;
 
 namespace FastGTD.CustomerTests
 {
     [TestFixture]
     public class EndToEndUITests
     {
+        private Application _app;
+
+        [TearDown]
+        public void CleanUp()
+        {
+            _app.Kill();
+        }
+
         [Test]
-        public void AddingItemToInbox()
+        public void AddingItemToInboxByPressingReturnKey()
         {
             string new_item = Guid.NewGuid().ToString();
             
-            var app = Application.Launch("FastGTD.exe");
-            var inbox_window = app.GetWindow("InBox");
-            AddItem(inbox_window, new_item);
-            app.WaitWhileBusy();
+            _app = Application.Launch("FastGTD.exe");
+            var inbox_window = _app.GetWindow("InBox");
+            InputNewItemInTextBox(inbox_window, new_item);
+            PressReturnKey(inbox_window);
+            _app.WaitWhileBusy();
 
             AssertListHasItem(inbox_window, new_item);
+        }
 
-            app.Kill();
+        [Test]
+        public void AddingItemToInboxByClickingButton()
+        {
+            string new_item = Guid.NewGuid().ToString();
+
+            _app = Application.Launch("FastGTD.exe");
+            var inbox_window = _app.GetWindow("InBox");
+            InputNewItemInTextBox(inbox_window, new_item);
+            ClickAddButton(inbox_window);
+            _app.WaitWhileBusy();
+
+            AssertListHasItem(inbox_window, new_item);
+        }
+
+        private static void ClickAddButton(Window inbox_window)
+        {
+            inbox_window.Get<Button>(SearchCriteria.ByText("Add")).Click();
+        }
+
+        private static void PressReturnKey(Window inbox_window)
+        {
+            inbox_window.KeyIn(KeyboardInput.SpecialKeys.RETURN);
+        }
+
+        private static void InputNewItemInTextBox(Window inbox_window, string new_item)
+        {
+            inbox_window.Focus();
+            inbox_window.Enter(new_item);
         }
 
         private static void AssertListHasItem(Window inbox_window, string new_item)
         {
             var list_view = (ListView) inbox_window.GetMultiple(SearchCriteria.ByControlType(typeof(ListView)))[0];
             Assert.That(list_view.Rows, Has.Some.Matches<ListViewRow>(i => i.Cells[0].Text == new_item));
-        }
-
-        private static void AddItem(Window inbox_window, string new_item)
-        {
-            inbox_window.Focus();
-            inbox_window.Enter(new_item);
-            inbox_window.KeyIn(KeyboardInput.SpecialKeys.RETURN);
         }
     }
 }

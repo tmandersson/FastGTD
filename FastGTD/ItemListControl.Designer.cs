22864408991d1327744558a4b5ce3fcd6aa4fb41
Thomas M. Andersson <ltmandersson@gmail.com>
2011-01-16 22:17:31 +0100

    * Made sure it is possible to use Design view for ItemListControl.

--- FastGTD/ItemListControl.Designer.cs
+++ FastGTD/ItemListControl.Designer.cs
@@ -1,4 +1,5 @@
-﻿using FastGTD.Domain;
+﻿using System.Windows.Forms;
+using FastGTD.Domain;
 
 namespace FastGTD
 {
@@ -30,67 +31,67 @@ namespace FastGTD
         /// </summary>
         private void InitializeComponent()
         {
-            this.ToActionButton = new System.Windows.Forms.Button();
-            this.DeleteButton = new System.Windows.Forms.Button();
-            this.AddButton = new System.Windows.Forms.Button();
-            this.TextBox = new System.Windows.Forms.TextBox();
-            this.ListView = new System.Windows.Forms.ListView();
+            this._to_action_button = new System.Windows.Forms.Button();
+            this._delete_button = new System.Windows.Forms.Button();
+            this._add_button = new System.Windows.Forms.Button();
+            this._text_box = new System.Windows.Forms.TextBox();
+            this._list_view = new System.Windows.Forms.ListView();
             this._new_items_header = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // _to_action_button
             // 
-            this.ToActionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.ToActionButton.Location = new System.Drawing.Point(15, 391);
-            this.ToActionButton.Name = "ToActionButton";
-            this.ToActionButton.Size = new System.Drawing.Size(75, 23);
-            this.ToActionButton.TabIndex = 9;
-            this.ToActionButton.Text = "To &Action";
-            this.ToActionButton.UseVisualStyleBackColor = true;
+            this._to_action_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this._to_action_button.Location = new System.Drawing.Point(15, 391);
+            this._to_action_button.Name = "ToActionButton";
+            this._to_action_button.Size = new System.Drawing.Size(75, 23);
+            this._to_action_button.TabIndex = 9;
+            this._to_action_button.Text = "To &Action";
+            this._to_action_button.UseVisualStyleBackColor = true;
             // 
             // _delete_button
             // 
-            this.DeleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.DeleteButton.Location = new System.Drawing.Point(351, 38);
-            this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
-            this.DeleteButton.TabIndex = 8;
-            this.DeleteButton.Text = "Delete";
-            this.DeleteButton.UseVisualStyleBackColor = true;
+            this._delete_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._delete_button.Location = new System.Drawing.Point(351, 38);
+            this._delete_button.Name = "DeleteButton";
+            this._delete_button.Size = new System.Drawing.Size(75, 23);
+            this._delete_button.TabIndex = 8;
+            this._delete_button.Text = "Delete";
+            this._delete_button.UseVisualStyleBackColor = true;
             // 
             // _add_button
             // 
-            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.AddButton.Location = new System.Drawing.Point(351, 9);
-            this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(75, 23);
-            this.AddButton.TabIndex = 6;
-            this.AddButton.Text = "Add";
-            this.AddButton.UseVisualStyleBackColor = true;
+            this._add_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._add_button.Location = new System.Drawing.Point(351, 9);
+            this._add_button.Name = "AddButton";
+            this._add_button.Size = new System.Drawing.Size(75, 23);
+            this._add_button.TabIndex = 6;
+            this._add_button.Text = "Add";
+            this._add_button.UseVisualStyleBackColor = true;
             // 
             // _text_box
             // 
-            this.TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            this._text_box.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.TextBox.Location = new System.Drawing.Point(15, 12);
-            this.TextBox.Name = "TextBox";
-            this.TextBox.Size = new System.Drawing.Size(330, 20);
-            this.TextBox.TabIndex = 5;
+            this._text_box.Location = new System.Drawing.Point(15, 12);
+            this._text_box.Name = "TextBox";
+            this._text_box.Size = new System.Drawing.Size(330, 20);
+            this._text_box.TabIndex = 5;
             // 
             // _list_view
             // 
-            this.ListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            this._list_view.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.ListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this._list_view.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this._new_items_header});
-            this.ListView.FullRowSelect = true;
-            this.ListView.Location = new System.Drawing.Point(3, 67);
-            this.ListView.Name = "ListView";
-            this.ListView.Size = new System.Drawing.Size(432, 318);
-            this.ListView.TabIndex = 7;
-            this.ListView.UseCompatibleStateImageBehavior = false;
-            this.ListView.View = System.Windows.Forms.View.Details;
+            this._list_view.FullRowSelect = true;
+            this._list_view.Location = new System.Drawing.Point(3, 67);
+            this._list_view.Name = "ListView";
+            this._list_view.Size = new System.Drawing.Size(432, 318);
+            this._list_view.TabIndex = 7;
+            this._list_view.UseCompatibleStateImageBehavior = false;
+            this._list_view.View = System.Windows.Forms.View.Details;
             // 
             // _new_items_header
             // 
@@ -100,11 +101,11 @@ namespace FastGTD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.ToActionButton);
-            this.Controls.Add(this.DeleteButton);
-            this.Controls.Add(this.AddButton);
-            this.Controls.Add(this.TextBox);
-            this.Controls.Add(this.ListView);
+            this.Controls.Add(this._to_action_button);
+            this.Controls.Add(this._delete_button);
+            this.Controls.Add(this._add_button);
+            this.Controls.Add(this._text_box);
+            this.Controls.Add(this._list_view);
             this.Name = "ItemListControl";
             this.Size = new System.Drawing.Size(438, 444);
             this.ResumeLayout(false);
--- FastGTD/ItemListControl.cs
+++ FastGTD/ItemListControl.cs
@@ -1,24 +1,29 @@
 ﻿using System.Windows.Forms;
-using FastGTD.Domain;
 
 namespace FastGTD
 {
     public partial class ItemListControl: UserControl
     {
+        private ListView _list_view;
+        private TextBox _text_box;
+        private Button _add_button;
+        private Button _to_action_button;
+        private Button _delete_button;
+
         public ItemListControl()
         {
             InitializeComponent();
         }
 
-        public ListView ListView { get; private set; }
-        public TextBox TextBox { get; private set; }
-        public Button AddButton { get; private set; }
-        public Button ToActionButton { get; private set; }
-        public Button DeleteButton { get; private set; }
+        public ListView ListView { get { return _list_view; } }
+        public TextBox TextBox { get { return _text_box; } }
+        public Button AddButton { get { return _add_button; } }
+        public Button ToActionButton { get { return _to_action_button; } }
+        public Button DeleteButton { get { return _delete_button; } }
 
         public void HideToActionButton()
         {
-            ToActionButton.Visible = false;
+            _to_action_button.Visible = false;
         }
     }
 }
